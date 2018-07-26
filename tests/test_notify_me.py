fb56f3ab9f3817f5dc617ef03e393ec7ceaaadbf
John Hossler <john.m.hossler@gmail.com>
2018-07-25 20:07:39 -0500

    feat(notify): basic notify functionality implemented (#1)
    * Added some basic notify functionality, added a script file where the basic notify_me script would go. Need a way to auto load the configuration for the cli command, but whatever for now
    
    * Added travis file to run tests

---
+++ tests/test_notify_me.py
@@ -0,0 +1,11 @@
+from unittest.mock import patch
+
+from notify_me.script import notify_me
+
+
+class TestNotifyMeSuite:
+    @patch('notify_me.script.requests')
+    def test_send_message(self, requests_mock):
+        notify_me('message', {'url': '127.0.0.1'})
+
+        requests_mock.post.assert_called_once_with('127.0.0.1', data={'message': 'message'})
