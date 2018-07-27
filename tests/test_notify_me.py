c6d50b0c8265f2a4da1faf6f0c756582d40af882
John Hossler <john.m.hossler@gmail.com>
2018-07-27 10:06:36 -0500

    Added cli_script functionality, so it can be called via cli and will load a 'default' config. Still need to have it add the default config if it is not there (#2)

--- tests/test_notify_me.py
+++ tests/test_notify_me.py
@@ -1,11 +1,11 @@
 from unittest.mock import patch
 
-from notify_me.script import notify_me
+from notify_me.notify_me import notify_me
 
 
 class TestNotifyMeSuite:
-    @patch('notify_me.script.requests')
+    @patch('notify_me.notify_me.requests')
     def test_send_message(self, requests_mock):
         notify_me('message', {'url': '127.0.0.1'})
 
-        requests_mock.post.assert_called_once_with('127.0.0.1', data={'message': 'message'})
+        requests_mock.post.assert_called_once_with('127.0.0.1', json={'message': 'message'})
